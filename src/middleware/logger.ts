bcc8c5cc3030440514b79f3efac323659c967a05
Arnaud Coral <arco51@gmail.com>
2024-01-22 21:16:52 +0100

    chore: logger cleanup

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,13 +1,14 @@
 import { type Context, type Next } from 'koa'
-import type Koa from 'koa';
 import logger from '../config/loggingConfig';
 
-const logMiddleware: Koa.Middleware = async (ctx: Context, next: Next) => {
+async function logMiddleware(ctx: Context, next: Next): Promise<void> {
     const start = Date.now();
     await next();
     const ms = Date.now() - start;
 
-    logger.info(`${ctx.method} ${ctx.url} - ${ms}ms`, { statusCode: ctx.status });
+    logger.info(`${ctx.method} ${ctx.url} - ${ms}ms`, {
+        statusCode: ctx.status,
+    });
 };
 
 export default logMiddleware;
